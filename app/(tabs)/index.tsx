c9e2881bc795f4b52de26a47ee43d24653fc8dd7
BraunRumpler <javierluis15975@hotmail.com>
2024-04-28 20:16:45 -0500

    template-mobile

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,43 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useFonts } from "expo-font";
+import { Button, Pressable, TouchableOpacity } from "react-native";
 
 export default function TabOneScreen() {
+  const [fontsLoaded, fontError] = useFonts({
+    RobotoSlab: require("../../assets/fonts/RobotoSlab-Light.ttf"),
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View>
+      <Text
+        style={{ fontFamily: "RobotoSlab" }}
+        className="text-xl font-extrabold"
+      >
+        Tab One
+      </Text>
+      <Text style={{ fontFamily: "RobotoSlab" }} className="text-xl">
+        Tab One
+      </Text>
+      <Text
+        style={{ fontFamily: "RobotoSlab" }}
+        className="text-3xl font-black"
+      >
+        hola
+      </Text>
+      <TouchableOpacity>
+        <Text>Hola</Text>
+      </TouchableOpacity>
+      <Pressable>
+        <Text
+          style={{ fontFamily: "RobotoSlab" }}
+          className="font-extrabold text-2xl bg-black text-white p-2"
+        >
+          hola
+        </Text>
+      </Pressable>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
---
+++ nativewind-env.d.ts
@@ -0,0 +1,2 @@
+// para que no salga error en las clases a la hora de dar clases
+/// <reference types="nativewind/types" />
